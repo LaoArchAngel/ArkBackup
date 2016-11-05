8c0a86a4df76276bc56ab5d26b5a28e004547c10
Lao <lao@nexverse.net>
2016-11-05 00:05:10 -0600

    Initial add of SaveWatcher

---
+++ ArkBackup/SaveWatcher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ArkBackup
+{
+    class SaveWatcher
+    {
+        /// <summary>
+        /// Watcher checking when the the save changes.
+        /// </summary>
+        private readonly FileSystemWatcher _watcher = new FileSystemWatcher();
+
+        /// <summary>
+        /// Number of backups to keep.
+        /// </summary>
+        private readonly int _backups = 20;
+
+        public SaveWatcher()
+        {
+            DirectoryInfo saveDir = new DirectoryInfo(".");
+            IEnumerable<FileInfo> saveFiles = saveDir.GetFiles("*.ark", SearchOption.TopDirectoryOnly);
+            FileInfo save = saveFiles.OrderBy(saveFile => saveFile.Length).First();
+
+            _watcher.Filter = save.Name;
+            _watcher.NotifyFilter = NotifyFilters.LastWrite;
+            _watcher.Changed += SaveChanged;
+        }
+
+        public SaveWatcher(int backups) : this()
+        {
+            _backups = backups;
+        }
+
+        private void SaveChanged(object source, FileSystemEventArgs args)
+        {
+            
+        }
+
+
+    }
+}
