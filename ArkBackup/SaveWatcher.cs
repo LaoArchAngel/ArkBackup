5c5d9deff61ff851a90e59ff5f0e19df939e0578
Lao <lao@nexverse.net>
2016-11-05 00:44:39 -0600

    Using AppDomain to get current path, JIC

--- ArkBackup/SaveWatcher.cs
+++ ArkBackup/SaveWatcher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,7 +20,7 @@ namespace ArkBackup
 
         public SaveWatcher()
         {
-            DirectoryInfo saveDir = new DirectoryInfo(".");
+            DirectoryInfo saveDir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
             IEnumerable<FileInfo> saveFiles = saveDir.GetFiles("*.ark", SearchOption.TopDirectoryOnly);
             FileInfo save = saveFiles.OrderBy(saveFile => saveFile.Length).First();
 
