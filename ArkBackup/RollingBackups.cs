1ec92ea710297e7f27d8797bdb3eb56b418c23e3
Lao <lao@nexverse.net>
2016-11-06 11:26:17 -0700

    Resharper Cleanup.

--- ArkBackup/RollingBackups.cs
+++ ArkBackup/RollingBackups.cs
@@ -7,24 +7,27 @@ using SevenZip;
 namespace ArkBackup
 {
     /// <summary>
-    /// Creates and prunes backups.
+    ///     Creates and prunes backups.
     /// </summary>
     internal class RollingBackups
     {
         /// <summary>
-        /// A list of our current backups.  The key is the time stamp of creation
-        /// </summary>
-        private readonly Dictionary<long, FileInfo> _backups = new Dictionary<long, FileInfo>();
-
-        /// <summary>
-        /// The number of backups to keep.
+        ///     The number of backups to keep.
         /// </summary>
         private readonly int _backupCount;
 
         /// <summary>
-        /// Creates a new RollingBackups instance specifying the number of backup files to keep.
+        ///     A list of our current backups. The key is the time stamp of creation
         /// </summary>
-        /// <param name="backups">The number of backup files to keep.  The oldest are deleted.</param>
+        private readonly Dictionary<long, FileInfo> _backups = new Dictionary<long, FileInfo>();
+
+        /// <summary>
+        ///     Creates a new RollingBackups instance specifying the number of
+        ///     backup files to keep.
+        /// </summary>
+        /// <param name="backups">
+        ///     The number of backup files to keep. The oldest are deleted.
+        /// </param>
         public RollingBackups(int backups = 20)
         {
             _backupCount = backups;
@@ -33,16 +36,14 @@ namespace ArkBackup
         }
 
         /// <summary>
-        /// Prunes backups that are missing or 
+        ///     Prunes backups that are missing or
         /// </summary>
         private void PruneBackups()
         {
             PruneMissing();
 
             if (_backups.Count <= _backupCount)
-            {
                 return;
-            }
 
             IEnumerable<long> toRemove = _backups.Keys.OrderBy(key => key);
             toRemove = toRemove.Skip(_backupCount);
@@ -51,24 +52,22 @@ namespace ArkBackup
             {
                 _backups[key].Delete();
                 _backups.Remove(key);
-            }            
+            }
         }
 
         /// <summary>
-        /// Prunes all missing backups from our current list.
+        ///     Prunes all missing backups from our current list.
         /// </summary>
         private void PruneMissing()
         {
             var missing = _backups.Where(backup => !backup.Value.Exists).Select(backup => backup.Key);
 
             foreach (var missingKey in missing)
-            {
                 _backups.Remove(missingKey);
-            }
         }
 
         /// <summary>
-        /// Loads all current backup files into our list for management.
+        ///     Loads all current backup files into our list for management.
         /// </summary>
         private void LoadCurrentBackups()
         {
@@ -77,19 +76,20 @@ namespace ArkBackup
 
             foreach (var backup in currentBackups)
             {
-                long date = Convert.ToInt64(backup.Name.Split('.', '_')[1]);
+                var date = Convert.ToInt64(backup.Name.Split('.', '_')[1]);
                 _backups.Add(date, backup);
             }
         }
 
         /// <summary>
-        /// Creates a .7z file with all save <paramref name="files"/> compressed.
+        ///     Creates a .7z file with all save <paramref name="files" />
+        ///     compressed.
         /// </summary>
         /// <param name="files">Files to be included in the backup</param>
         public void CreateBackup(IEnumerable<FileInfo> files)
         {
-            SevenZipCompressor.SetLibraryPath(Path.Combine(@"C:\Program Files\7-Zip", @"7z.dll"));
-            SevenZipCompressor compressor = new SevenZipCompressor();
+            SevenZipBase.SetLibraryPath(Path.Combine(@"C:\Program Files\7-Zip", @"7z.dll"));
+            var compressor = new SevenZipCompressor();
 
             var timestamp = TimeStamp();
 
@@ -100,9 +100,7 @@ namespace ArkBackup
             Console.WriteLine("Backing up files:");
 
             foreach (var fileInfo in files)
-            {
                 Console.WriteLine(fileInfo.FullName);
-            }
 
             compressor.CompressFiles(archiveName, files.Select(info => info.FullName).ToArray());
             _backups.Add(timestamp, new FileInfo(archiveName));
@@ -113,12 +111,15 @@ namespace ArkBackup
         }
 
         /// <summary>
-        /// Returns the year as a 64-bit integer in the format <c>yyyyMMddHHmm</c>
+        ///     Returns the year as a 64-bit integer in the format
+        ///     <c>yyyyMMddHHmm</c>
         /// </summary>
-        /// <returns>The current time stamp, up to the minute.</returns>
+        /// <returns>
+        ///     The current time stamp, up to the minute.
+        /// </returns>
         private static long TimeStamp()
         {
-            long timestamp = (long) DateTime.Now.Year*100000000;
+            var timestamp = (long) DateTime.Now.Year*100000000;
             timestamp += DateTime.Now.Month*1000000;
             timestamp += DateTime.Now.Day*10000;
             timestamp += DateTime.Now.Hour*100;
