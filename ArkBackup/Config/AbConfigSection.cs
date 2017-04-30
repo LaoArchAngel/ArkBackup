5eb1dd70a4e7a53fc5b996dda9d83ff8f558ccb8
Lao <lao@nexverse.net>
2017-04-29 19:26:09 -0600

    Created AbConfigSection
    Allow users to specify save name, path, rolling save count, and delay.

---
+++ ArkBackup/Config/AbConfigSection.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+
+namespace ArkBackup.Config
+{
+    public class AbConfigSection : ConfigurationSection
+    {
+        [ConfigurationProperty("name", DefaultValue = "TheIsland", IsRequired = false)]
+        public string Name  
+        {
+            get => (string) this["name"];
+            set => this["name"] = value;
+        }
+
+        [ConfigurationProperty("path", IsRequired = true)]
+        public string Path
+        {
+            get => (string) this["path"];
+            set => this["path"] = value;
+        }
+
+        [ConfigurationProperty("saves", IsRequired = false, DefaultValue = 10)]
+        public int Saves
+        {
+            get => (int) this["saves"];
+            set => this["saves"] = value;
+        }
+
+        [ConfigurationProperty("delay", IsRequired = false, DefaultValue = 10)]
+        public int Delay
+        {
+            get => (int) this["delay"];
+            set => this["delay"] = value;
+        }
+    }
+}
